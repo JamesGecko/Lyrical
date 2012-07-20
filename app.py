83d542e825c0b885ef68c438da5a620a3461e7b3
JamesGecko <jamesgecko@gmail.com>
2012-07-20 18:44:32 -0400

    Sending lyrics to projector window works.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import sys
 import os, errno
 from PySide.QtGui import QApplication, QMainWindow, QWidget
 from PySide.QtGui import QDesktopWidget
-from PySide.QtCore import *
+from PySide import QtCore
 from gui.ui_mainwindow import Ui_MainWindow
 from gui.ui_projector import Ui_Projector
 from model import Database, Song
@@ -18,30 +18,27 @@ class LyricalProjector(QWidget, Ui_Projector):
         QWidget.__init__(self)
         self.setupUi(self)
 
-class LyricalControl(QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        QMainWindow.__init__(self)
-        self.setupUi(self)
-
-    @Slot()
-    def foo(self):
-        windows[1].show()
-        windows[0].hide()
-
-    def update(slide):
+    def update(self, slide):
         '''set title and content to slide
         '''
-        pass
+        self.title.setText(slide.title)
+        self.content.setText(slide.content)
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    windows = []
-    windows.append(LyricalControl())
-    windows.append(LyricalProjector())
-    windows[0].show()
-    #windows[1].showFullScreen()
-    #windows[1].show()
+class LyricalControl(QMainWindow, Ui_MainWindow):
+    def __init__(self, projector_window):
+        QMainWindow.__init__(self)
+        self.projector = projector_window
+        self.setupUi(self)
 
+        self.lyrics.clicked.connect(self.update_screen)
+
+    @QtCore.Slot()
+    def update_screen(self):
+        title = self.song_list.currentItem().text()
+        content = self.lyrics.currentItem().text()
+        self.projector.update(Slide(title, content))
+
+def get_database():
     data_directory = os.path.expanduser('~/.lyrical')
     try:
         os.makedirs(data_directory)
@@ -49,10 +46,21 @@ if __name__ == '__main__':
         if e.errno != errno.EEXIST:
             raise
     db = Database(data_directory + '/songs.sqlite')
+    return db
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    projector = LyricalProjector()
+    controller = LyricalControl(projector)
+    controller.show()
+    projector.show()
+
+    db = get_database()
 
     song = Song()
-    windows[0].lyrics.insertItems(0, song.lyrics_list())
-    windows[0].song_list.addItem(song.title)
+    controller.lyrics.insertItems(0, song.lyrics_list())
+    controller.song_list.addItem(song.title)
 
     desktop = QDesktopWidget()
     if desktop.screenCount() < 2:
