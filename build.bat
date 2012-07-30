d045ebba90efd29f48bd4999e94be5ccc4d687a4
JamesGecko <jamesgecko@gmail.com>
2012-07-30 16:51:26 -0400

    Fixing error in build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 pyside-uic gui/mainwindow.ui > gui/ui_mainwindow.py
 pyside-uic gui/projector.ui > gui/ui_projector.py
-pyside-uic gui/editor.ui > gui/ui_editor.py
+pyside-uic gui/edit.ui > gui/ui_editor.py
 pyside-uic gui/picker.ui > gui/ui_picker.py
